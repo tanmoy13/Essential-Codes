306c940f4a88729489be8e9b6ec674975d2d7c8f
tanmoy13 <tanmoytapos01@gmail.com>
2017-10-05 07:42:14 +0600

    updated

--- String Algorithm/Suffix Array/Suffix_Array_nlogn_Halim.cpp
+++ String Algorithm/Suffix Array/Suffix_Array_nlogn_Halim.cpp
@@ -60,70 +60,124 @@ using namespace std;
 //bool check(int N,int pos){return (bool)(N & (1<<pos));}
 /*------------------------------------------------*/
 
+
 #define mx 100005
 
+int SA[mx],tempSA[mx];
+int RA[mx],tempRA[mx];
+int c[mx];
+int LCP[mx],PLCP[mx],Phi[mx];
+//int P[18][mx];
 char str[mx];
-int SA[mx],temp_SA[mx]; //SA[] = Suffix Array
-int RA[mx],temp_RA[mx]; //RA[] = Rank for Suffix
-int cnt_sort[mx];
 int n;
 
 void counting_sort(int k)
 {
     int maxi=max(300,n);
-    ms(cnt_sort,0);
-    for(int i=0;i<n;i++)
+    ms(c,0);
+    for(int i=0; i<n; i++)
     {
         int a=i+k<n?RA[i+k]:0;
-        cnt_sort[a]++;
+        c[a]++;
     }
-    for(int i=0,sum=0;i<maxi;i++)
+    for(int i=0,sum=0; i<maxi; i++)
     {
-        int x=cnt_sort[i];
-        cnt_sort[i]=sum;
+        int x=c[i];
+        c[i]=sum;
         sum+=x;
     }
 
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
         int a=SA[i]+k<n?RA[SA[i]+k]:0;
-        temp_SA[cnt_sort[a]]=SA[i];
-        cnt_sort[a]++;
+        int b=c[a];
+        c[a]++;
+        tempSA[b]=SA[i];
     }
 
-    for(int i=0;i<n;i++)
-        SA[i]=temp_SA[i];
-
+    for(int i=0; i<n; i++)
+        SA[i]=tempSA[i];
 }
 
 void build_Suffix_Array()
 {
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
         RA[i]=str[i];
         SA[i]=i;
     }
 
-    for(int k=1;k<n;k*=2)
+    for(int k=1; k<n; k*=2)
     {
         counting_sort(k);
         counting_sort(0);
-        temp_RA[SA[0]]=0;
         int r=0;
-        for(int i=1;i<n;i++)
+        tempRA[SA[0]]=r=0;
+        for(int i=1; i<n; i++)
         {
-            int a=SA[i];
             if(RA[SA[i]]==RA[SA[i-1]] && RA[SA[i]+k]==RA[SA[i-1]+k])
-                temp_RA[SA[i]]=r;
+                tempRA[SA[i]]=r;
             else
-                temp_RA[SA[i]]=++r;
+                tempRA[SA[i]]=++r;
         }
-        for(int i=0;i<n;i++)
-            RA[i]=temp_RA[i];
-        if(RA[SA[n-1]]==n-1)
-            break;
+        for(int i=0; i<n; i++)
+        {
+            RA[i]=tempRA[i];
+        }
+        if(RA[SA[n-1]]==n-1) break;
     }
 }
+
+void build_LCP()
+{
+    Phi[SA[0]]=-1;
+    for(int i=1; i<n; i++)
+        Phi[SA[i]]=SA[i-1];
+    for(int i=0,L=0; i<n; i++)
+    {
+        if(Phi[i]==-1)
+        {
+            PLCP[i]=0;
+            continue;
+        }
+        while(str[i+L]==str[Phi[i]+L]) L++;
+        PLCP[i]=L;
+        L=max(L-1,0);
+    }
+
+    for(int i=0; i<n; i++)
+        LCP[i]=PLCP[SA[i]];
+}
+
+int find_pattern(int len)
+{
+    int lo=0,hi=n-1;
+    while(lo<hi)
+    {
+        int mid=(lo+hi)/2;
+        int res=strncmp(str+SA[mid],pattern,len);
+        if(res>=0)
+            hi=mid;
+        else
+            lo=mid+1;
+    }
+    ll ret=lo;
+    if(strncmp(str+SA[ret],pattern,len)!=0) return 0;
+    lo=0,hi=n-1;
+    while(lo<hi)
+    {
+        int mid=(lo+hi)/2;
+        int res=strncmp(str+SA[mid],pattern,len);
+        if(res>0)
+            hi=mid;
+        else
+            lo=mid+1;
+    }
+
+    if(strncmp(str+SA[hi],pattern,len)!=0) hi--;
+    return hi-ret+1;
+}
+
 
 int main()
 {
